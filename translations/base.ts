a7bebcdb3332dd268ddb7b06c78228bd98c0982e
Johan Temmerman <github@jeetee.net>
2016-01-07 00:03:57 +0100

    Added translation for High-Accuracy Mode Files updates to TS version 2.1 (Linguist in Qt5.4\mingw491)

--- translations/base.ts
+++ translations/base.ts
@@ -1,30 +1,30 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1">
 <context>
     <name>UltraStarAnnotate</name>
     <message>
-        <location filename="../UltraStarAnnotate.qml" line="15"/>
+        <location filename="../UltraStarAnnotate.qml" line="+15"/>
         <source>UltraStar Annotator</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltraStarAnnotate.qml" line="16"/>
+        <location line="+1"/>
         <source>Annotate Score for UltraStart Export</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltraStarAnnotate.qml" line="35"/>
+        <location line="+20"/>
         <source>Make Line Break</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltraStarAnnotate.qml" line="42"/>
+        <location line="+7"/>
         <source>Make Golden Note</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltraStarAnnotate.qml" line="49"/>
+        <location line="+7"/>
         <source>Make Freestyle Note</source>
         <translation type="unfinished"></translation>
     </message>
@@ -32,99 +32,104 @@
 <context>
     <name>UltrastarExport</name>
     <message>
-        <location filename="../UltrastarExport.qml" line="25"/>
-        <source>Minimum MuseScore Version 2.1.0 required for export</source>
+        <location filename="../UltrastarExport.qml" line="+25"/>
+        <source>Minimum MuseScore Version 2.0.1 required for export</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="28"/>
+        <location line="+3"/>
         <source>Select a score before exporting.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="97"/>
+        <location line="+68"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="112"/>
+        <location line="+15"/>
         <source>Warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="130"/>
+        <location line="+18"/>
         <source>Please choose a directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="143"/>
+        <location line="+12"/>
         <source>Ultrastar Export</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="160"/>
+        <location line="+17"/>
         <source>Player 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="166"/>
-        <location filename="../UltrastarExport.qml" line="198"/>
+        <location line="+6"/>
+        <location line="+32"/>
         <source>Instrument</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="176"/>
-        <location filename="../UltrastarExport.qml" line="208"/>
+        <location line="-22"/>
+        <location line="+32"/>
         <source>Voice</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="182"/>
+        <location line="-26"/>
         <source>Duet</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="192"/>
+        <location line="+10"/>
         <source>Player 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="215"/>
+        <location line="+23"/>
         <source>Select export directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="226"/>
+        <location line="+10"/>
+        <source>High Accuracy Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+7"/>
         <source>Export</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="234"/>
+        <location line="+8"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="250"/>
+        <location line="+16"/>
         <source>Exporting .txt File.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="254"/>
+        <location line="+4"/>
         <source>Exporting .mp3 File.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="256"/>
+        <location line="-2"/>
         <source></source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="262"/>
+        <location line="+10"/>
         <source>Score must have a title.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../UltrastarExport.qml" line="281"/>
+        <location line="+19"/>
         <source>Score must have a composer.</source>
         <translation type="unfinished"></translation>
     </message>
