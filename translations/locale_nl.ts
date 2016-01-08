fb38e406c055efe847037a7acd5299443c3b2fc6
Johan Temmerman <github@jeetee.net>
2016-01-08 01:24:49 +0100

    Added Dutch translation

---
+++ translations/locale_nl.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>UltraStarAnnotate</name>
+    <message>
+        <location filename="../UltraStarAnnotate.qml" line="+15"/>
+        <source>UltraStar Annotator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Annotate Score for UltraStart Export</source>
+        <translation>Annoteer partituren voor de UltraStar Export plugin</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Make Line Break</source>
+        <translation>Regeleinde</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Make Golden Note</source>
+        <translation>&quot;Gouden&quot; noot</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Make Freestyle Note</source>
+        <translation>Freestyle noot</translation>
+    </message>
+</context>
+<context>
+    <name>UltrastarExport</name>
+    <message>
+        <location filename="../UltrastarExport.qml" line="+25"/>
+        <source>Minimum MuseScore Version 2.0.1 required for export</source>
+        <translation>Exporter vereist minimaal versie 2.0.1 van MuseScore</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Select a score before exporting.</source>
+        <translation>Open een partituur alvorens te exporteren.</translation>
+    </message>
+    <message>
+        <location line="+68"/>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Warning</source>
+        <translation>Waarschuwing</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Please choose a directory</source>
+        <translation>Gelieve een map te kiezen</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Ultrastar Export</source>
+        <translation>UltraStar Export</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Player 1</source>
+        <translation>Speler 1</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <location line="+32"/>
+        <source>Instrument</source>
+        <translation>Instrument</translation>
+    </message>
+    <message>
+        <location line="-22"/>
+        <location line="+32"/>
+        <source>Voice</source>
+        <translation>Stem</translation>
+    </message>
+    <message>
+        <location line="-26"/>
+        <source>Duet</source>
+        <translation>Duet</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Player 2</source>
+        <translation>Speler 2</translation>
+    </message>
+    <message>
+        <location line="+23"/>
+        <source>Select export directory</source>
+        <translation>Selecteer export map</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>High Accuracy Mode</source>
+        <translation>Hoge Nauwkeurigheidsmode</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Export</source>
+        <translation>Exporteren</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Exporting .txt File.</source>
+        <translation>.txt bestand exporteren</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Exporting .mp3 File.</source>
+        <translation>.mp3 bestand exporteren</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Score must have a title.</source>
+        <translation>Partituur dient een titel te hebben.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Score must have a composer.</source>
+        <translation>Partituur dient een componist te hebben.</translation>
+    </message>
+</context>
+</TS>
