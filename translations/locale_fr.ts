474901a13e95fb204f08ae7187d2bc966b2bb5f3
Joseph Eoff <joseph.eoff@molear.de>
2017-11-12 19:50:54 +0100

    Signed-off-by: Joseph Eoff <joseph.eoff@molear.de>

---
+++ translations/locale_fr.ts
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>Ms::MuseScore</name>
+    <message>
+        <location filename="../UltrastarExport.qml" line="+161"/>
+        <location line="+29"/>
+        <source>Instrument</source>
+        <translation>Instrument</translation>
+    </message>
+</context>
+<context>
+    <name>PrefsDialogBase</name>
+    <message>
+        <location line="+33"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location line="-195"/>
+        <source>No score open.
+This plugin requires an open score to run.
+</source>
+        <translation>Pas de partion ouverte.
+Ce plugin nécessite l&apos;ouverture d&apos;une partition pour fonctionner.
+</translation>
+    </message>
+</context>
+<context>
+    <name>UltraStarAnnotate</name>
+    <message>
+        <location filename="../UltraStarAnnotate.qml" line="+15"/>
+        <source>UltraStar Annotator</source>
+        <translation>Annotateur UltraStar</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Annotate Score for UltraStar Export</source>
+        <translation>Annote la partition pour l&apos;exportation UltraStar</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Make Line Break</source>
+        <translation>Faire un saut de ligne</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Make Golden Note</source>
+        <translation>Faire une note dorée</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Make Freestyle Note</source>
+        <translatorcomment>Je pense que free style s&apos;utilise dans le sens artistique</translatorcomment>
+        <translation>Faire une note artistique</translation>
+    </message>
+</context>
+<context>
+    <name>UltrastarExport</name>
+    <message>
+        <location filename="../UltrastarExport.qml" line="-3"/>
+        <source>Minimum MuseScore Version %1 required for export</source>
+        <translation>Version minimale de MuseScore %1 requise pour l&apos;exportation</translation>
+    </message>
+    <message>
+        <location line="+71"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Warning</source>
+        <translation>Attention</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Please choose a directory</source>
+        <translation>Veuillez choisir un répertoire</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Ultrastar Export</source>
+        <translation>Exporter en Ultrastar</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Player 1</source>
+        <translation>Joueur 1</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <location line="+29"/>
+        <source>Voice</source>
+        <translation>Voix</translation>
+    </message>
+    <message>
+        <location line="-23"/>
+        <source>Duet</source>
+        <translation>Duo</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Player 2</source>
+        <translation>Joueur 2</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>Select export directory</source>
+        <translation>Sélectionnez le répertoire d&apos;exportation</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>High Accuracy Mode</source>
+        <translation>Mode haute précision</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Exporting .txt File.</source>
+        <translation>Exportation du fichier .txt.</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Exporting .mp3 File.</source>
+        <translation>Exportation du fichier .mp3.</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Score must have a title.</source>
+        <translation>La partition doit avoir un titre.</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Score must have a composer.</source>
+        <translation>La partition doit avoir un compositeur.</translation>
+    </message>
+</context>
+</TS>
